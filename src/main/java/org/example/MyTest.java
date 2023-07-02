73bbfff4ac100ae8b04bc17b7a5b31055d860441
Pavel_Vatsek <pvatsek@gmail.com>
2023-07-02 23:50:28 +0300

    process

---
+++ src/main/java/org/example/MyTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+
+public class MyTest {
+    public static void main(String[] args) {
+        String a = "M";
+
+        if (a == "M"){
+            System.out.println("true");
+        }
+    }
+}
